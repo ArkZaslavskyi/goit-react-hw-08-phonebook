893b91628ed1b0442758cc520f37987baf2bf19b
ArkZaslavskyi <ark.zaslavskyi@gmail.com>
2022-12-28 23:43:54 +0200

    added Navigation and AuthNav

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -1,10 +1,12 @@
+import { AuthNav } from "components/AuthNav";
+import { Navigation } from "components/Navigation";
 import css from "./AppBar.module.css";
 
 export const AppBar = () => {
   return (
     <header className={css.header}>
-      Navigation (Home, Contacts)
-      AuthNav (Register, Login) | UserMenu
+      <Navigation />
+      <AuthNav /> {/** | UserMenu */}
     </header>
   );
 };
