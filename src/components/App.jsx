377ec9ac6f09ece831ac7566b1b24ad73db8ea63
ArkZaslavskyi <ark.zaslavskyi@gmail.com>
2022-12-28 23:41:13 +0200

    added all routes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,20 +5,11 @@ export const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
-
+        <Route index element={<div>Home</div>} />
+        <Route path="contacts" element={<div>Contacts</div>} />
+        <Route path="register" element={<div>Register</div>} />
+        <Route path="login" element={<div>Log In</div>} />
       </Route>
     </Routes>
-    // <div
-    //   style={{
-    //     height: '100vh',
-    //     display: 'flex',
-    //     justifyContent: 'center',
-    //     alignItems: 'center',
-    //     fontSize: 40,
-    //     color: '#010101'
-    //   }}
-    // >
-    //   React homework template
-    // </div>
   );
 };
