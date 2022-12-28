21ac4ef22ccf08781b58f5907abb28c5d08f54d7
ArkZaslavskyi <ark.zaslavskyi@gmail.com>
2022-12-29 01:06:24 +0200

    pages structure added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,4 @@
+import { Contacts, Home, Login, Register } from "pages";
 import { Route, Routes } from "react-router-dom";
 import { Layout } from "./Layout";
 
@@ -5,10 +6,10 @@ export const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
-        <Route index element={<div>Home</div>} />
-        <Route path="contacts" element={<div>Contacts</div>} />
-        <Route path="register" element={<div>Register</div>} />
-        <Route path="login" element={<div>Log In</div>} />
+        <Route index element={<Home />} />
+        <Route path="contacts" element={<Contacts/>} />
+        <Route path="register" element={<Register />} />
+        <Route path="login" element={<Login />} />
       </Route>
     </Routes>
   );
---
+++ src/pages/index.js
@@ -0,0 +1,6 @@
+import Contacts from './Contacts';
+import Home from './Home';
+import Login from './Login';
+import Register from './Register';
+
+export { Home, Contacts, Register, Login };
