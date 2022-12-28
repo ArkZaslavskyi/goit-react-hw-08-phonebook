166bb3d3d016ef3862913e883a453104c9fb117f
ArkZaslavskyi <ark.zaslavskyi@gmail.com>
2022-12-28 22:17:46 +0200

    add name of base router

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import './styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={'/goit-react-hw-08-phonebook'}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
