f00a165742a155ba220f7e7d73999a8086f42b20
ArkZaslavskyi <ark.zaslavskyi@gmail.com>
2022-12-29 03:06:10 +0200

    base contacts operation created

---
+++ src/redux/contacts/operations.js
@@ -0,0 +1,60 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://connections-api.herokuapp.com';
+
+/*
+ * Get all user contacts
+​ * GET @ ​/contacts
+ * headers: Authorization: Bearer token
+*/
+
+export const getContacts = createAsyncThunk(
+  'contacts/getAll',
+  async (_, thunkAPI) => {
+    try {
+      const { data } = await axios.get('​/contacts');
+      return data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
+
+/*
+ * Create a new contact
+​ * POST @ ​/contacts
+ * headers: Authorization: Bearer token
+ * Request body: {name, number}
+*/
+
+export const addContact = createAsyncThunk(
+  'contacts/addContact',
+  async (contact, thunkAPI) => {
+    try {
+      const { data } = await axios.post('​/contacts', contact);
+      return data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
+
+/*
+ * Delete contact
+​ * DELETE @ ​/contacts​/{contactId}
+ * headers: Authorization: Bearer token
+ * path: contactId
+*/
+
+export const deleteContact = createAsyncThunk(
+  'contacts/deleteContact',
+  async (contactId, thunkAPI) => {
+    try {
+      const { data } = await axios.post(`/contacts/${contactId}`);
+      return data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
