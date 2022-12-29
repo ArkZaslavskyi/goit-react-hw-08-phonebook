1b5a381ddf58407046ddcb2763ced529401f613e
ArkZaslavskyi <ark.zaslavskyi@gmail.com>
2022-12-29 12:51:00 +0200

    LoginForm created

---
+++ src/components/LoginForm/LoginForm.jsx
@@ -0,0 +1,35 @@
+import { useDispatch } from "react-redux";
+import { register } from "redux/auth/operations";
+import css from "./LoginForm.module.css";
+
+export const RegisterForm = () => {
+  const dispatch = useDispatch();
+
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    const form = e.currentTarget;
+    dispatch(
+      register({
+        email: form.elements.email.value,
+        password: form.elements.password.value
+      })
+    );
+    
+    form.reset();
+  };
+
+  return (
+    <form className={css.form} onSubmit={handleSubmit} autoComplete="off" >
+      <label className={css.label}>
+        Email
+        <input type="email" name="email" />
+      </label>
+      <label className={css.label}>
+        Password
+        <input type="text" name="password" />
+      </label>
+      <button type="submit">Register</button>
+    </form>
+  );
+};
