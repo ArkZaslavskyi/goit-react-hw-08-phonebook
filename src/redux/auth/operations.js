2e434b2925e9a96294f58837be8492318531f374
ArkZaslavskyi <ark.zaslavskyi@gmail.com>
2022-12-29 03:04:43 +0200

    auth operations structure, register function

---
+++ src/redux/auth/operations.js
@@ -0,0 +1,39 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://connections-api.herokuapp.com';
+
+/*
+ * Create a new user
+​ * POST @ /users​/signup
+ * Request body: {name, email, password}
+*/
+
+export const register = createAsyncThunk(
+  'auth/register',
+  async (credentials, thunkAPI) => {
+    try {
+      const { data } = await axios.post('/users​/signup', credentials);
+      console.log(data);
+      return data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
+
+/*
+ * Login user
+​ * POST @ /users/login
+ * Request body: {email, password}
+*/
+
+export const login = createAsyncThunk('auth/login');
+
+/*
+ * Log out user
+​ * POST @ /users/logout
+ * headers: Authorization: Bearer token
+*/
+
+export const logout = createAsyncThunk('auth/logout');
