155c4af8f4e29949cf549fdf07aea49803967032
ArkZaslavskyi <ark.zaslavskyi@gmail.com>
2022-12-30 23:52:48 +0200

    loading  to Contacts page

--- src/pages/Contacts.js
+++ src/pages/Contacts.js
@@ -1,9 +1,25 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { selectIsError, selectIsLoading } from 'redux/contacts/selectors';
+import { getContacts } from 'redux/contacts/operations';
+
 import { ContactForm } from 'components/ContactForm';
+import { ContactList } from 'components/ContactList';
 
 const Contacts = () => {
+  const dispatch = useDispatch();
+
+  const isLoading = useSelector(selectIsLoading);
+  const isError = useSelector(selectIsError);
+
+  useEffect(() => {
+    dispatch(getContacts());
+  }, [dispatch]);
+
   return (
     <>
       <ContactForm />
+      {!isLoading && !isError && <ContactList />}
     </>
   );
 };
