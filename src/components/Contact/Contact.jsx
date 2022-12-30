529e1676c3dfe22e18e1f6ed7e524cd6af4437ac
ArkZaslavskyi <ark.zaslavskyi@gmail.com>
2022-12-30 23:53:24 +0200

    Contact added

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,22 @@
+import { MdClose } from 'react-icons/md';
+import { useDispatch } from 'react-redux';
+import { deleteContact } from 'redux/contacts/operations';
+import css from "./Contact.module.css";
+
+export const Contact = ({ id, name, number }) => {
+  const dispatch = useDispatch();
+
+  const handleDelete = id => {
+    dispatch(deleteContact(id))
+  };
+
+  return (
+    <li className={css.listItem}>
+      <span className={css.contactName}>{name}</span>
+      <span className={css.contactNumber}>{number}</span>
+      <button className={css.deleteBtn} type="button" onClick={() => handleDelete(id)}>
+        <MdClose size={24} />
+      </button>
+    </li>
+  );
+};
