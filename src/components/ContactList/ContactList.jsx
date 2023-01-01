4bf5b0bb8af0b7f84c1c3f7e790fc5edbe547ea1
ArkZaslavskyi <ark.zaslavskyi@gmail.com>
2023-01-02 00:59:41 +0200

    added filter to ContactList

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,15 +1,19 @@
 import { useSelector } from "react-redux";
-import { selectContacts } from "redux/contacts/selectors";
+import { selectContacts, selectFilter } from "redux/contacts/selectors";
 import { Contact } from "components/Contact";
+import { getFilteredContacts } from "utils/getFilteredContacts";
 import css from "./ContactList.module.css";
 
 export const ContactList = () => {
-  const items = useSelector(selectContacts);
+  const filter = useSelector(selectFilter);
+  const contacts = useSelector(selectContacts);
+
+  const filteredContacts = getFilteredContacts(contacts, filter);
 
   return (
     <ul className={css.list}>
-      {items.map((item) =>
-        (<Contact key={item.id} {...item} />)
+      {filteredContacts.map((contact) =>
+        (<Contact key={contact.id} {...contact} />)
       )}
     </ul>
   );
