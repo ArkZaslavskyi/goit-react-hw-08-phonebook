abb8c2ea176bd6c195c4c5a82f679336ed1d1cce
ArkZaslavskyi <ark.zaslavskyi@gmail.com>
2023-01-01 03:34:07 +0200

    add refreshing user before routing

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,9 @@
 import { useEffect, lazy } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { refreshUser } from "redux/auth/operations";
 import { Route, Routes } from "react-router-dom";
 import { Layout } from "./Layout";
+import { selectIsRefreshing } from "redux/auth/selectors";
 
 const HomePage = lazy(() => import("../pages/Home"));
 const RegisterPage = lazy(() => import("../pages/Register"));
@@ -11,19 +12,25 @@ const ContactsPage = lazy(() => import("../pages/Contacts"));
 
 export const App = () => {
   const dispatch = useDispatch();
+  const isRefreshing = useSelector(selectIsRefreshing);
 
   useEffect(() => {
     dispatch(refreshUser());
   }, [dispatch]);
-  
+
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<HomePage />} />
-        <Route path="contacts" element={<ContactsPage />} />
-        <Route path="register" element={<RegisterPage />} />
-        <Route path="login" element={<LoginPage />} />
-      </Route>
-    </Routes>
+    isRefreshing
+      ? (<div style={{ fontWeight: 700, color: 'red' }}>
+          Requesting user...
+        </div>)
+      : (<Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<HomePage />} />
+            <Route path="contacts" element={<ContactsPage />} />
+            <Route path="register" element={<RegisterPage />} />
+            <Route path="login" element={<LoginPage />} />
+          </Route>
+        </Routes>
+      )
   );
 };
